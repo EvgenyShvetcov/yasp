48be14ee83d25897f518265b8997f8869fa7278d
Евгений Швецов <67862313+EvgenyShvetcov@users.noreply.github.com>
2021-10-20 02:20:16 +0300

    fix

--- test-task/src/components/atoms/DifferenceTable.jsx
+++ test-task/src/components/atoms/DifferenceTable.jsx
@@ -5,8 +5,16 @@ import Up from "./../../icons/Up.svg";
 export const DifferenceTable = ({ difference }) => {
   return (
     <>
-      <div className={`diff ` + (difference > 0 ? "" : "-bad")}>
-        <img className="plus" src={difference < 0 ? Down : Up} alt="" />
+      <div
+        className={
+          `diff ` +
+          (difference !== 0 &&
+            ((difference > 0 && "-good") || (difference < 0 && "-bad")))
+        }
+      >
+        {difference !== 0 && (
+          <img className="plus" src={difference < 0 ? Down : Up} alt="" />
+        )}
         <div style={{ marginLeft: "2px" }}>
           {difference > 0 ? "+" + difference : difference}
         </div>
